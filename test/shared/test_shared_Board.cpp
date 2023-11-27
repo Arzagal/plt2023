58469d75b6faa9ffa37875da5cbfed69330294c5
louis <louis.fay@ensea.fr>
2023-11-27 09:38:00 +0200

    Writing render UT

--- test/shared/test_shared_Board.cpp
+++ test/shared/test_shared_Board.cpp
@@ -1,26 +1,23 @@
 //
 // Created by louis on 11/7/23.
 //
-//
-// Created by louis on 11/7/23.
-//
+
 
 
 #include <boost/test/unit_test.hpp>
 #include "../../src/shared/state/Board.h"
 #include "../../src/shared/state.h"
 
-namespace state {
-    BOOST_AUTO_TEST_CASE(firstBoard){
-            Board* board = new Board();
-            int num = 0;
-            Player player = Player(num);
-            int location = 0;
-            board -> random_init();
-            board-> move_player(player.get_number(),location);
-            std::vector <int> neighbours = board-> get_neighbours(player.get_number());
-            board-> get_effect(location);
-            int loc = board-> get_location(player.get_number());
-            BOOST_CHECK_EQUAL(loc, 0);
-    }
+using namespace state;
+BOOST_AUTO_TEST_CASE(firstBoard){
+        Board* board = new Board();
+        int num = 0;
+        Player player = Player(num);
+        int location = 0;
+        board -> random_init();
+        board-> move_player(player.get_number(),location);
+        std::vector <int> neighbours = board-> get_neighbours(player.get_number());
+        board-> get_effect(location);
+        int loc = board-> get_location(player.get_number());
+        BOOST_CHECK_EQUAL(loc, 0);
 }
