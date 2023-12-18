2fc0cba476a79f9b17735a50fb1e5606ebd8c12e
louis <louis.fay@ensea.fr>
2023-12-18 11:57:05 +0200

    Adding ai dia

--- test/shared/test_shared_Darknlight.cpp
+++ test/shared/test_shared_Darknlight.cpp
@@ -6,8 +6,13 @@
 #include "../../src/shared/state/Darknlight.h"
 using namespace state;
 BOOST_AUTO_TEST_CASE(firstDarknlightTest){
-        Darknlight darkn = Darknlight();
-        darkn.get_id ();
-        darkn.play_effect ();
-        BOOST_CHECK(1);
+    Darknlight darkn = Darknlight();
+    darkn.set_id(12);
+    darkn.set_card_type(0);
+    int id = darkn.get_id ();
+    darkn.play_effect();
+    int type = darkn.get_card_type();
+    BOOST_CHECK_EQUAL(id, 12);
+    BOOST_CHECK_EQUAL(type, 0);
+    BOOST_CHECK(1);
 }
