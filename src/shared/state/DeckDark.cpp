7ce9f4a40b0b5fcc9733f1cfe60db9c7b87714de
louis <louis.fay@ensea.fr>
2023-12-19 18:14:15 +0200

    Adding Cards deck

--- src/shared/state/DeckDark.cpp
+++ src/shared/state/DeckDark.cpp
@@ -6,7 +6,8 @@
 namespace state{
     DeckDark::DeckDark() {
         for(int i =0; i<16; i++){
-            Darknlight Card = new Darknlight();
+            Darknlight* Card = new Darknlight(i-1);
+            this->deck.push_back(Card);
         }
     }
     void DeckDark:: remove (int card_id){}
