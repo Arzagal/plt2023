4a3a4cf5e249aacc2d4d184d5aad6cad30f1ed78
louis <louis.fay@ensea.fr>
2023-12-20 12:16:28 +0200

    Adding Cards deck

--- src/shared/state/DeckDark.cpp
+++ src/shared/state/DeckDark.cpp
@@ -2,6 +2,8 @@
 // Created by louis on 10/24/23.
 //
 #include "DeckDark.h"
+#include <algorithm>
+#include <random>
 
 namespace state{
     DeckDark::DeckDark() {
@@ -9,7 +11,16 @@ namespace state{
             Darknlight* Card = new Darknlight(i-1);
             this->deck.push_back(Card);
         }
+
     }
     void DeckDark:: remove (int card_id){}
-    Darknlight* DeckDark:: draw (){}
+    Darknlight* DeckDark:: draw (){
+        std::random_device rd;
+        std::mt19937 g(rd());
+
+        std::shuffle(deck.begin(), deck.end(), g);
+        Darknlight* res =  deck.at(deck.size()-1);
+        deck.pop_back();
+        return res;
+    }
 }
