e011b8edbac3a833b9a16f69ed005c755b593107
louis <louis.fay@ensea.fr>
2024-01-10 17:37:43 +0200

    Improved code clarity and removes useless includes

--- config.sh
+++ config.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-
+## We create a repository to compile the project in
+# We empty it to avoid any problem
 rm -r build
-
 mkdir build
-
 cd build
 
+# We use CMake for the compilation
 cmake ..
-
 make
 
 #firefox https://www.youtube.com/watch?v=dQw4w9WgXcQ &
 
+#This runs the test
 make code-coverage
 
+#This open the code coverage summary
 firefox code-coverage/index.html &
