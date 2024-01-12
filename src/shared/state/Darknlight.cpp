2f2a8bd0906b1ed5fdd08945c894b71a745d045f
Krustybe <thomas.fuzellier@outlook.fr>
2024-01-12 10:11:10 +0100

    Mise a jour des cartes Lights et Darks

--- src/shared/state/Darknlight.cpp
+++ src/shared/state/Darknlight.cpp
@@ -16,67 +16,96 @@ namespace state{
             int choice;
             switch (card_id) {
                 case 1:
-                    std::cout << "Vous avez piochez la carte Dynamite." << std::endl;
                     alea = rand() % 6;
-                    std::cout << "Le resultat du lancé est : " << alea << "." << std::endl;
-                    std::cout << "Effet spécial : Infliger 3 points de dégâts à tous les joueurs à la localisation 3." << std::endl;
                     for (int player =0; player < game->get_number_player(); player++) {
                         if (game->get_player_location(player) == alea) {
                             game->add_wound(player, 3);
-                            std::cout << "3 points de dégâts infligés au joueur " << player << "." << std::endl;
                         }
                     }
                     break;
                 case 2:
                 case 3:
-                    std::cout << "Vous avez pioché la carte Chauve-Souris Vampire." << std::endl;
                     if (target != NULL) {
                         game->add_wound(target, 2);
-                        std::cout << "Vous infligez 2 points de dégâts au joueur " << target << "." << std::endl;
-                        // Vous pouvez également infliger 2 points de dégâts au joueur qui a pioché la carte
                         game->heal(game->get_active_player(), 2);
-                        std::cout << "Vous vous êtes soigné de 2 points de vie en piochant la carte." << std::endl;
                     }
                     break;
                 case 4:
                 case 5:
-                    std::cout << "Vous avez pioché la carte Quitte ou Double." << std::endl;
                     choice = rand() % 2;
                     if (choice == 0){
                             game->add_wound(target, 5);
-                            std::cout << "Vous infligez 2 points de dégâts au joueur " << target<< "." << std::endl;
                     }else {
                         game->add_wound(game->get_active_player(), 5);
-                        std::cout << "Vous vous êtes soigné de 2 points de vie en piochant la carte." << std::endl;
                     }
 
                     break;
                 case 6:
                 case 7:
-                case 8: case 10:
-                    std::cout << "Vous avez piochez la carte Mitrailleuse Funeste." << std::endl;
-                    std::cout << "Effet spécial : Infliger 1 point de dégât à tous les joueurs à la localisation 8." << std::endl;
+                case 8:
+                case 10:
                     for (int player =0; player <game->get_number_player(); player++) {
                         if (game->get_player_location(player) == 3) {
                             game->add_wound(player,1);
-                            std::cout << "1 point de dégât infligé au joueur " << player << "." << std::endl;
                         }
                     }
                     break;
                 case 9:
                 case 11:
                 case 12:
-                    std::cout << "Vous avez piochez la carte Poupée Démoniaque." << std::endl;
                         game->add_wound(target, 2);
-                        std::cout << "Vous infligez 2 points de dégâts au joueur " << target << "." << std::endl;
                     break;
 
                 default:
                     break;
             }
-            }
+        }
         else if(this->card_type == 0){
-            game->heal(target, 1);
+            srand(time(NULL));
+            int alea;
+            int choice;
+            switch (card_id) {
+                case 1:
+                    alea = rand() % 6;
+                    for (int player =0; player < game->get_number_player(); player++) {
+                        if (game->get_player_location(player) == alea) {
+                            game->heal(player, 3);
+                        }
+                    }
+                    break;
+                case 2:
+                case 3:
+                    game->heal(game->get_active_player(), rand() % 6);
+                    break;
+                case 4:
+                case 5:
+                    choice = rand() % 2;
+                    if (choice == 0){
+                            game->heal(target, 2);
+                    }else {
+                        game->heal(game->get_active_player(), 2);
+                    }
+
+                    break;
+                case 6:
+                case 7:
+                case 8: case 10:
+                    for (int player =0; player <game->get_number_player(); player++) {
+                        if (game->get_player_location(player) == 2) {
+                            game->add_wound(player,1);
+                            game->heal(player, 2);
+                        }
+                    }
+                    break;
+                case 9:
+                case 11:
+                case 12:
+                        game->heal(game->get_active_player(), 2);
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 
